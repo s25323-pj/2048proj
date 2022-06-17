a297e7ff4a3440fb9d2b863c6d1d164cc0f7d215
s25323-pj <92204632+s25323-pj@users.noreply.github.com>
2022-06-17 19:40:22 +0200

    Cell class add

---
+++ src/com/company/Cell.java
@@ -0,0 +1,93 @@
+package com.company;
+
+import java.awt.Color;
+
+
+public class Cell
+{
+    int value;
+
+    Color cellColor;
+
+    public Cell()
+    {
+        value = 0;
+    }
+
+    public Cell( int number )
+    {
+        value = number;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+
+
+    public void setValue( int value )
+    {
+        this.value = value;
+    }
+
+
+    public String toString()
+    {
+        return "" + value;
+    }
+
+
+    public void setColor()
+    {
+        if ( this.getValue() == 2 )
+        {
+            cellColor = new Color( 238, 228, 218 );
+        }
+        else if ( this.getValue() == 4 )
+        {
+            cellColor = new Color( 237, 224, 200 );
+        }
+        else if ( this.getValue() == 8 )
+        {
+            cellColor = new Color( 242, 177, 121 );
+        }
+        else if ( this.getValue() == 16 )
+        {
+            cellColor = new Color( 245, 149, 99 );
+        }
+        else if ( this.getValue() == 32 )
+        {
+            cellColor = new Color( 246, 124, 95 );
+        }
+        else if ( this.getValue() == 64 )
+        {
+            cellColor = new Color( 246, 94, 59 );
+        }
+        else if ( this.getValue() == 128 )
+        {
+            cellColor = new Color( 237, 207, 114 );
+        }
+        else if ( this.getValue() == 256 )
+        {
+            cellColor = new Color( 237, 204, 97 );
+        }
+        else if ( this.getValue() == 512 )
+        {
+            cellColor = new Color( 237, 200, 80 );
+        }
+        else if ( this.getValue() == 1024 )
+        {
+            cellColor = new Color( 237, 197, 63 );
+        }
+        else
+        {
+            cellColor = new Color( 237, 194, 46 );
+        }
+    }
+    public Color getColor()
+    {
+        this.setColor();
+        return cellColor;
+    }
+
+}
